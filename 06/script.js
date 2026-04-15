b0041e430cf5d8a2b38c2f116aa76227e585e1fa
hiroki goda <hiroz000qop@gmail.com>
2026-04-15 16:35:09 +0900

    done 06

--- 06/script.js
+++ 06/script.js
@@ -1,15 +1,62 @@
 'use strict';
 
-console.log('Hello World!!');
+// console.log('Hello World!!');
 
-const profile = {name: 'OJK', weight: 85};
-profile.bloodtype = 'A';
-profile['height'] = 181;
-profile.name = 'historio';
+// const profile = {name: 'OJK', weight: 85};
+// profile.bloodtype = 'A';
+// profile['height'] = 181;
+// profile.name = 'historio';
 
-const obj = {a: 'A', b: 'B', c: 'C'};
+// const obj = {a: 'A', b: 'B', c: 'C'};
 
-for (const prop in obj){
-    console.log(prop);
-    console.log(obj[prop]);
+// for (const prop in obj){
+//     console.log(prop);
+//     console.log(obj[prop]);
+// }
+
+const table = document.querySelector('table');
+const tr = document.createElement('tr');
+// const th = document.createElement('th');
+const headers = ['名前', 'タイプ', '体重', '特技'];
+
+// th.textContent = '名前';
+
+// tr.appendChild(th);
+// table.appendChild(tr);
+
+for (const h of headers){
+    const th = document.createElement('th');
+    th.textContent = h;
+    tr.appendChild(th);
+}
+
+table.appendChild(tr);
+
+const evList = [
+  { name: 'サンダース', type: 'でんき', weight: 24.5, ability: 'ちくでん' },
+  { name: 'ブースター', type: 'ほのお', weight: 25.0, ability: 'もらいび' },
+  { name: 'エーフィ', type: 'エスパー', weight: 26.5, ability: 'シンクロ' },
+  { name: 'ブラッキー', type: 'あく', weight: 27.0, ability: 'シンクロ' },
+  { name: 'リーフィア', type: 'くさ', weight: 25.5, ability: 'リーフガード' },
+  { name: 'ニンフィア', type: 'フェアリー', weight: 23.5, ability: 'メロメロボディ' },
+  { name: 'グレイシア', type: 'こおり', weight: 25.9, ability: 'ゆきがくれ' },
+  { name: 'シャワーズ', type: 'みず', weight: 29.0, ability: 'ちょすい' },
+];
+
+// for (const ev of evList){
+//     const tr = document.createElement('tr');
+//     const td = document.createElement('td');
+//     td.textContent = ev.name;
+//     tr.appendChild(td);
+//     table.appendChild(tr);
+// }
+
+for (const ev of evList){
+    const tr = document.createElement('tr');
+    for (const prop in ev){
+        const td = document.createElement('td');
+        td.textContent = ev[prop];
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
 }

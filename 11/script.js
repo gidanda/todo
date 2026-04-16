c784a0f93a686e9b569883befcb828a6db37f427
hiroki goda <hiroz000qop@gmail.com>
2026-04-16 14:47:59 +0900

    11 done

--- 11/script.js
+++ 11/script.js
@@ -12,3 +12,99 @@ const list = [
   'images/09.jpg',
   'images/10.jpg',
 ];
+
+const img = document.getElementById('main');
+const [leftArrow, rightArrow] = document.getElementsByTagName('span');
+
+let imgN = 0;
+
+const p = document.createElement('p');
+p.style.color = 'lightgray';
+
+const div = document.getElementById('photo');
+div.insertBefore(p, img.nextElementSibling);
+
+function setImage() {
+  img.src = list[imgN];
+  p.textContent = `${imgN + 1} / ${list.length}`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setImage();
+});
+
+leftArrow.addEventListener('click', () => {
+  imgN -= 1;
+  if (imgN < 0) {
+    imgN = 0;
+  }
+  setImage();
+});
+
+rightArrow.addEventListener('click', () => {
+  imgN += 1;
+  if (imgN >= list.length) {
+    imgN = list.length - 1;
+  }
+  setImage();
+});
+
+// let count = 0;
+// document.body.style.color = 'white';
+
+// const countID = setInterval(() => {
+//     document.body.textContent = count;
+//     count += 1;
+//     if (count > 5){
+//         clearInterval(countID);
+//         document.body.textContent += ' stopped!!';
+//     }
+// }, 1000);
+
+// const playButton = document.createElement('button');
+// playButton.textContent = '自動再生する';
+// div.appendChild(playButton);
+
+// let autoplay;
+
+// playButton.addEventListener('click', () => {
+//     autoplay = setInterval(() => {
+//         imgN += 1;
+//     if (imgN >= list.length){
+//         imgN = 0;
+//     }
+//     setImage();
+//     }, 500);
+    
+// });
+
+// const stopButton = document.createElement('button');
+// stopButton.textContent = '停止する';
+// div.appendChild(stopButton);
+
+// stopButton.addEventListener('click', () => {
+//     clearInterval(autoplay);
+// });
+
+const button = document.createElement('button');
+button.textContent = '自動再生する';
+div.appendChild(button);
+
+let autoplay = false;
+
+button.addEventListener('click', () => {
+  if (autoplay == false) {
+    button.textContent = '停止する';  // ボタンをトグル
+    autoplay = setInterval(() => {
+      imgN += 1;
+      if (imgN >= list.length) {
+        imgN = 0;
+      }
+      setImage();
+    }, 500);
+  } else {
+    clearInterval(autoplay);
+    autoplay = false;
+    button.textContent = '自動再生する';  // ボタンをトグル
+  }
+});

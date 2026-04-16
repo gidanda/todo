45be19eb8e4efaa0d931ebb61c17c7ac2184f9d2
hiroki goda <hiroz000qop@gmail.com>
2026-04-16 09:01:47 +0900

    done 08

--- 08/script.js
+++ 08/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('Hello World!!');
+
+const lis = document.querySelectorAll('li');
+
+const lisArray = Array.from(lis);  // NodeListを配列に変換
+const newLis = lisArray.map((li) => li.textContent + '!!');
+console.log(newLis);
+
+const a = document.getElementById('link');
+console.log(a);
+
+const p = a.parentElement;
+console.log(p);
+
+const img = a.firstElementChild;
+console.log(img);
